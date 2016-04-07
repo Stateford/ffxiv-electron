6ae1e67a8ab187c75a0cd02bfd5ff80e3ef29930
idietmoran <idietmoran@gmail.com>
2016-04-07 18:13:05 -0400

    init

--- app.js
+++ app.js
@@ -5,6 +5,6 @@
 const electron = require('electron');
 
 electron.app.on('ready', () => {
-    const mainWindow = new electron.BrowserWindow({width: 800, height: 800});
+    const mainWindow = new electron.BrowserWindow({width: 400, height: 400});
     mainWindow.loadURL('file://' + __dirname + '/client/index.html');
 });
